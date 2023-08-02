3fe61b33fde6476d611f805ce9a44c879503cd37
YauCode <yauheni.baikou88@gmail.com>
2023-08-02 11:17:48 +0300

    Music Generation AI Done

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -16,13 +16,6 @@ export const tools = [
         bgColor: "bg-violet-500/10",
     },
     {
-        label: 'Music Generation',
-        icon: Music,
-        href: '/music',
-        color: "text-emerald-500",
-        bgColor: "bg-emerald-500/10",
-    },
-    {
         label: 'Image Generation',
         icon: ImageIcon,
         color: "text-pink-700",
@@ -35,6 +28,13 @@ export const tools = [
         color: "text-orange-700",
         bgColor: "bg-orange-700/10",
         href: '/video',
+    },
+    {
+        label: 'Music Generation',
+        icon: Music,
+        href: '/music',
+        color: "text-emerald-500",
+        bgColor: "bg-emerald-500/10",
     },
     {
         label: 'Code Generation',
