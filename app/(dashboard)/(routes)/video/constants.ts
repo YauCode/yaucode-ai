4d63a1a95181fec26ee282ce98169e2bc86eddc6
YauCode <yauheni.baikou88@gmail.com>
2023-08-02 11:44:05 +0300

    Video Generation AI Done

---
+++ app/(dashboard)/(routes)/video/constants.ts
@@ -0,0 +1,9 @@
+// To keeps form schema
+
+import * as z from "zod";
+
+export const formSchema = z.object({
+    prompt: z.string().min(1, {
+        message: "Video Prompt is required."
+    }),
+});
