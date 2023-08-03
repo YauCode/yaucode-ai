ccc9e797426645255821214da1ca3321a2f5a526
YauCode <yauheni.baikou88@gmail.com>
2023-08-03 15:56:52 +0300

    UI Updates

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -71,7 +71,7 @@ const Sidebar = ({
             <div className="px-3 py-2 flex-1">
                 <Link href="/dashboard" className="flex items-center pl-3 mb-14">
                     <div className="relative h-8 w-8 mr-4">
-                        <Image fill alt="Logo" src="/logo.png" />
+                        <Image fill alt="Logo" src="/main_logo.png" />
                     </div>
                     <h1 className={cn("text-2xl font-bold", montserrat.className)}>
                         YauCode-AI
